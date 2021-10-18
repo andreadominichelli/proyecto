65cf8cd14dd1f8d6d37d9855840ce2f0bb8e3fb9
Andrea Pereira Dominichelli <andrea_msn_12@hotmail.com>
2021-10-17 21:18:50 -0300

    cambioeentrega5
    carrito

--- js/init.js
+++ js/init.js
@@ -47,14 +47,18 @@ function start () {
   location.replace ("login.html");
   } 
 }
+function mostrarUsuario (){
+document.getElementById("nombreUsuario").innerHTML= sessionStorage.getItem("logeado")
+document.getElementById("nombreUsuario").style.color = "white"
+};
 
 
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-/* document.addEventListener("DOMContentLoaded", function(e){
-  document.getElementById("nombreUsuario").innerHTML= sessionStorage.getItem("logeado")
-document.getElementById("nombreUsuario").style.color = "white"
-});
- */
+ document.addEventListener("DOMContentLoaded", function(e){
+  mostrarUsuario();
+  });
+ 
+ 
 
